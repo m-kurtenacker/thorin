e144f3820e4a7885c381e80a1f22a8f1bf982a61
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2022-07-28 12:35:30 +0200

    Support for multiple input arguments.

--- src/thorin/transform/plugin_execute.cpp
+++ src/thorin/transform/plugin_execute.cpp
@@ -7,6 +7,7 @@ namespace thorin {
 void plugin_execute(Thorin& thorin) {
     World& world = thorin.world();
     world.VLOG("start plugin_execute");
+
     for (auto cont : world.copy_continuations()) {
         if (cont->is_intrinsic() && cont->intrinsic() == Intrinsic::Plugin) {
             void * function_handle = thorin.search_plugin_function(cont->name());
@@ -14,7 +15,7 @@ void plugin_execute(Thorin& thorin) {
                 world.ELOG("Plugin function not found for: {}", cont->name());
                 continue;
             }
-            auto plugin_function = (void*(*)(void*)) function_handle;
+            auto plugin_function = (void*(*)(size_t, void**)) function_handle;
 
             for (auto use : cont->copy_uses()) {
                 if (!use.def()->isa<App>()) {
@@ -23,17 +24,19 @@ void plugin_execute(Thorin& thorin) {
 
                 auto app = const_cast<App*>(use.def()->as<App>());
 
-                void * input = (void*) app->arg(1);
-                void * output = plugin_function(input);
-                if (input != output) {
-                    world.ELOG("Plugin changed stuff");
+                Def* input_array[app->num_args() - 2];
+                for (size_t i = 1, e = app->num_args() - 1; i < e; i++) {
+                    Def * input = const_cast<Def*>(app->arg(i));
+                    input_array[i - 1] = input;
                 }
+
+                void * output = plugin_function(app->num_args() - 2, (void **)input_array);
 
                 Continuation* y = world.continuation(world.fn_type({world.mem_type(), world.fn_type({world.mem_type()})}));
                 y->jump(y->param(1), {y->param(0)});
 
                 Continuation* x = world.continuation(world.fn_type({world.mem_type()}));
-                x->jump(app->arg(2), {x->param(0), y});
+                x->jump(app->arg(app->num_args() - 1), {x->param(0), y});
 
                 app->jump((Def*)output, {app->arg(0), x});
             }
